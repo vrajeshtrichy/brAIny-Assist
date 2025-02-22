1f81b40927938cee8bb4a066048c5b3a1165ba39
vrajeshtrichy <vrajeshtrichy@github.com>
2025-02-22 22:11:32 +0530

    Initial Commit

---
+++ brAIny-Assist/brAIny_AssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  brAIny_AssistApp.swift
+//  brAIny-Assist
+//
+//  Created by Rajesh Kanna V on 2025-02-22.
+//
+
+import SwiftUI
+
+@main
+struct brAIny_AssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ brAIny-AssistTests/brAIny_AssistTests.swift
@@ -0,0 +1,17 @@
+//
+//  brAIny_AssistTests.swift
+//  brAIny-AssistTests
+//
+//  Created by Rajesh Kanna V on 2025-02-22.
+//
+
+import Testing
+@testable import brAIny_Assist
+
+struct brAIny_AssistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
